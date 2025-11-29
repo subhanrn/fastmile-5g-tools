d2d1fac5cbaf5be1ab961b472f8c3a477449595b
nahal <subhanrn@outlook.com>
2025-11-29 21:43:37 +0700

    move code out of main.js

--- main.js
+++ main.js
@@ -1,55 +1,9 @@
 #!/usr/bin/env node
 
-const { parseArgs } = require('./lib/cli');
-const { mergeConfigWithArgs } = require('./lib/config');
-const { printHelp } = require('./lib/help');
-const {
-    handleLogin,
-    handleConfig,
-    handleReconnect,
-    handleRestart,
-    handleStatus,
-} = require('./lib/commands');
-
-async function main() {
-    let args = parseArgs();
-
-    if (args.help) {
-        printHelp();
-        process.exit(0);
-    }
-
-    // Merge saved config with command line arguments
-    // Command line args take precedence
-    args = mergeConfigWithArgs(args);
-
-    // Handle different commands
-    switch (args.command) {
-        case 'config':
-            handleConfig();
-            break;
-            
-        case 'login':
-            await handleLogin(args);
-            break;
-            
-        case 'reconnect':
-            await handleReconnect(args);
-            break;
-            
-        case 'restart':
-            await handleRestart(args);
-            break;
-            
-        case 'status':
-        default:
-            await handleStatus(args);
-            break;
-    }
-}
+const { run } = require('./lib/run');
 
 // Run the CLI
-main().catch(err => {
+run().catch(err => {
     const { colors } = require('./lib/constants');
     console.error(`${colors.red}Fatal error: ${err.message}${colors.reset}`);
     process.exit(1);
